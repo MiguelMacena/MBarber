429ef67523de5f4256475bb6eb4858c6a80f105a
MiguelMacena <miguelmacena1610@gmail.com>
2025-12-17 21:16:37 -0300

    chore: apply black formatting

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    #apps do projeto
+    # apps do projeto
     "apps.users",
     "apps.services",
     "apps.schedules",
